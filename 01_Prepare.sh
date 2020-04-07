6a8131d64092024b02924943fd6a897c25b03caa
Kuba Maroušek <kuba@ulmo.cz>
2020-04-07 08:50:34 +0200

    Spouštěcí skripty pro první fázi a celý proces

---
+++ 01_Prepare.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+cd "$(dirname $0)"
+
+cd cedr
+python3 LoadCedr.py
+cd ..
+
+cd czechinvest
+python3 LoadCzinvest.py
+cd ..
+
+cd dotinfo
+python3 LoadDotinfo.py
+cd ..
+
+cd eufondy
+python3 LoadEufondy.py
+cd ..
+
+cd szif
+python3 LoadSzif.py
+cd ..
