2ddf4f81b3a87b6830463469bd0e5aa423db131f
suchoss <suchoss@gmail.com>
2022-03-14 18:18:07 +0100

    adding command to scaffold db

--- Common/IntermediateDb/IntermediateDbContext.cs
+++ Common/IntermediateDb/IntermediateDbContext.cs
@@ -38,6 +38,8 @@ public class IntermediateDbContext : DbContext
                 .LogToConsole()
                 .Build();
         
+        var result = upgrader.PerformUpgrade();
+        
     }
 
 }
